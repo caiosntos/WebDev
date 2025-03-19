7614637a546bb159e8a74f963eed98787637a5d5
caiosntos <caiogiovanists@outlook.com>
2025-03-19 19:35:27 -0300

    main

--- WebContent/js/site.js
+++ WebContent/js/site.js
@@ -6,29 +6,72 @@ function validaFaleConosco(){
         return false;
     }
 
-    if(document.frmfaleconosco.txtfone==""){
+    if(document.frmfaleconosco.txtfone.value==""){
         alert("Preencha o seu número de telefone");
         document.frmfaleconosco.txtfone.focus();
         return false;
     }
 
-    if(document.frmfaleconosco.txtmail==""){
+    if(document.frmfaleconosco.txtemail.value==""){
         alert("Preencha o seu e-mail");
-        document.frmfaleconosco.txtmail.focus();
+        document.frmfaleconosco.txtemail.focus();
         return false;
     }
 
-    if(document.frmfaleconosco.selmotivo==""){
+    if(document.frmfaleconosco.selmotivo.value==""){
         alert("Preencha o motivo");
         document.frmfaleconosco.selmotivo.focus();
         return false;
     }
 
-    if(document.frmfaleconosco.txcomentario==""){
+    if(document.frmfaleconosco.txcomentario.value==""){
         alert("Preencha o comentário");
         document.frmfaleconosco.txcomentario.focus();
         return false;
     }
 
+    if(document.frmfaleconosco.selproduto.value==""){
+        alert("Preencha o produto corretamente.");
+        document.frmfaleconosco.selproduto.focus();
+        return false;
+    }
+
     return true;
+}
+
+function verificaMotivo(motivo){
+    var elemento = document.getElementById("opcaoProduto");
+
+    if(motivo=="PR"){
+        var select = document.createElement("select");
+        select.setAttribute("name", "selproduto");
+
+        var option = document.createElement("option");
+        option.setAttribute("value", "");
+
+        var texto = document.createTextNode("Escolha");
+        option.appendChild(texto);
+        select.appendChild(option);
+
+        
+        var option = document.createElement("option");
+        option.setAttribute("value", "FR");
+        var texto = document.createTextNode("Freezer");
+        option.appendChild(texto);
+        select.appendChild(option);
+
+        var option = document.createElement("option");
+        option.setAttribute("value", "GE");
+        var texto = document.createTextNode("Geladeira");
+        option.appendChild(texto);
+        select.appendChild(option); 
+
+        elemento.appendChild(select);
+
+        
+    }else{
+        if(elemento.firstChild){
+            elemento.removeChild(elemento.firstChild);
+        }
+    }
 }
